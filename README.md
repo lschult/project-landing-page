860d8e3c84b2d73cbac07132bea3e7d202aac97b
lschult <136801449+lschult@users.noreply.github.com>
2023-07-19 16:17:28 +0200

    Feat: completed landing page project with the javaScript file

--- README.md
+++ README.md
@@ -1,9 +1,59 @@
-# Landing Page Project
+# Project Title
 
-## Instructions
+UDACITY - Landing Page Project
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+# Summary
 
-To get started, open `js/app.js` and start building out the app's functionality
+This is the landing page project, which is part of the of the Udacity Nanodegree Front End Developer Program. It is based on the JavaScript/DOM course.
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Starter file with HTML/CSS code was provided by Udacity, and the project was completed using JavaScript in order to achieve a dynamic, multi section landing page. Certain modifications to the HTML and CSS structure were implemented, and a separate JavaScript file was created which made the static version of the landing page interactive.
+
+# Table of Contents
+
+- [Project Title](#project-title)
+- [Summary](#summary)
+- [Table of Contents](#table-of-contents)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Development](#development)
+- [Contribute](#contribute)
+- [License](#license)
+
+# Installation
+
+This project can be installed by cloning the repository through Github and downloading it onto a computer.
+
+# Usage
+
+By following the installation instructions, the projects' files can be opened on any code text editor of choice (such as VS Code).
+
+# Development
+
+This project consists of 4 distinct files:
+
+- index.html
+
+- js/app.js
+
+- css/styles.css
+
+- README.md
+
+  1.) Modifications within styles.css were performed in order to achieve the dynamic fucntionality which is visible on the project
+  2.) SLight modifications done to index.html (2 buttons and 2 sections were added)
+  3.) app.js contains the majority of the work which was performed. This includes:
+
+  - Created the Navbar by using the createNav() function, which looped through each section and built the appropriate li elements (including html text), which were appended to the the navbar.
+  - Made scrolling between sections smooth by using EventListener and the srcollTo fucntion.
+  - Highlighted the active section that is being viewed by the user by using the method getBoundingClientRect(), if/else statement, and establishing when the active section changes. In addition to the actual section being highlighted, the appropriate element in the navbar was modified with CSS to change accordingly.
+  - Added functionality to show the navbar when user is scrolling, and hide it when scrolling stops using setTimeout method.
+  - Additional button was created to toggle between sections being expanded or collapsed. This was achieved by using EventListener and if/else statement, showing different paragraph heights depending on which state is active.
+  - Created Scroll Top Button within the footer, and modified it by using EventListener and if/else to only be visible if the user scrolls below the fold of the page. Added functionality to smoothly transition to the top of the page when clicked.
+
+# Contribute
+
+Udacity
+
+# License
+
+© Udacity
--- js/app.js
+++ js/app.js
@@ -1,62 +1,174 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
+// Start Global Variables
+const navList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+const navMenu = document.querySelector(".navbar__menu");
+// End Global Variables
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+// Start build the nav
+function createNav(sections) {
+  sections.forEach((section) => {
+    // Create li elements which are making up the ul
+    const sectionId = section.getAttribute("id");
+    const sectionName = section.getAttribute("data-nav");
+    const navElement = document.createElement("li");
+    // The li element's HTML content is inserted
+    navElement.insertAdjacentHTML(
+      "afterbegin",
+      `<a href="#${section.id}" class="nav__link">${section.dataset.nav}</a>`
+    );
+    // li is appended to ul
+    navList.appendChild(navElement);
+    // Function for the scrolling behavior is called
+    scrollEvent(navElement, section);
+  });
+  // ul appended to nav
+  navMenu.appendChild(navList);
+}
+createNav(sections);
+//End build the nav
 
-/**
- * Define Global Variables
- * 
-*/
+// Start of Scroll to anchor ID using scrollTO event
+function scrollEvent(navElement, section) {
+  navElement.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({
+      top: section.offsetTop,
+      left: section.offsetLeft,
+      behavior: "smooth",
+    });
+  });
+}
+// End of Scroll to anchor ID using scrollTO event
 
+// Start of Set the Section class 'active' when it near to the top of viewport
+function activeSection(event) {
+  event.preventDefault();
+  // Anchors selected
+  const navItems = document.querySelectorAll(".nav__link");
+  sections.forEach((section, index) => {
+    // Access the sections's boundingRect
+    const area = section.getBoundingClientRect();
+    //Get minimum size of section in the viewport
+    const sectionFirstLinePosition = getSectionParagraphPosition(section);
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+    const minimumPartInView = window.innerHeight > sectionFirstLinePosition;
+    // A section is active when the minimum part of a section is in the view, and the end of the section is not above the user's viewport top and the next section is not in the viewport
+    const isActive =
+      minimumPartInView &&
+      area.bottom > 0 &&
+      !nextSectionParagraphInView(index);
 
+    if (isActive) {
+      section.classList.add("your-active-class");
+      navItems[index].classList.add("active_btn");
+      section.classList.add("active");
+    } else {
+      section.classList.remove("your-active-class");
+      navItems[index].classList.remove("active_btn");
+      section.classList.remove("active");
+    }
+  });
+}
 
+function nextSectionParagraphInView(sectionIndex) {
+  const nextSectionIndex = sectionIndex + 1;
+  const nextSection = sections[nextSectionIndex];
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+  if (nextSection === undefined) {
+    return false;
+  }
+  const inView = getSectionParagraphPosition(nextSection) < window.innerHeight;
+  return inView;
+}
 
-// build the nav
+function getSectionParagraphPosition(section) {
+  const pEl = section.querySelector("p");
+  const area = pEl.getBoundingClientRect();
+  // + 18 (font-size) so the first line in the next paragraph is visible
+  //TODO: get the font size of the paragraph programmatically
+  const position = area.top + 18;
+  return position;
+}
 
+document.addEventListener("scroll", function (event) {
+  activeSection(event);
+});
+// End of Set the Section class 'active' when it near to the top of viewport
 
-// Add class 'active' to section when near top of viewport
+// Start of Toggle the NavBar According to User Scroll Activity
+//Show navmenu by removing hidden nav class
+function displayNavmenu() {
+  const navBar = document.querySelector(".page__header");
+  navBar.classList.remove("hidden-nav");
+}
+//Hide navmenu by adding hidden nav class
+function hideNavmenu() {
+  const navBar = document.querySelector(".page__header");
+  navBar.classList.add("hidden-nav");
+}
 
+function manageActions() {
+  window.addEventListener("scroll", displayNavmenu);
 
-// Scroll to anchor ID using scrollTO event
+  let timeout;
+  //timeout runs after scrolling stops an change on navmenu is exceuted after timeout finishes
+  function resetTimeout() {
+    clearTimeout(timeout);
+    timeout = setTimeout(hideNavmenu, 1500);
+  }
+  //While user is scrolling timeout remains reset
+  window.addEventListener("scroll", resetTimeout);
+}
 
+manageActions();
+// End of Toggle the NavBar According to User Scroll Activity
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+//Function to expand/collapse sections
+sections.forEach((section) => {
+  const paragraph = section.querySelector(".section-paragraph");
+  //Button created in HTML called
+  const button = section.querySelector(".expand-paragraph");
 
-// Build menu 
+  if (button === null) {
+    return;
+  }
+  // click button to toggle expand or collapse of the section
+  button.addEventListener("click", () => {
+    section.classList.toggle("open");
+    // When the section is expanded, the entire paragraph is visible
 
-// Scroll to section on link click
+    if (section.classList.contains("open")) {
+      paragraph.style.maxHeight = paragraph.scrollHeight + "px";
+      button.innerHTML = "Read less";
+      // When the section is collapsed, only a specific height of the paragraph is visible
+    } else {
+      paragraph.style.maxHeight = `${310}px`;
+      button.innerHTML = "Read more";
+    }
+  });
+});
 
-// Set sections as active
+// Scroll to top button of the Landing Page using scrollTo
 
-
+function scrollTopBtn() {
+  //Button created in HTML called
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+  //Button visible as soon as user srolls underneath the fold of the page (on page load)
+  document.addEventListener("scroll", function () {
+    if (window.scrollY < window.innerHeight) {
+      scrollTopBtn.classList.remove("visible");
+    } else {
+      scrollTopBtn.classList.add("visible");
+    }
+  });
+  //ScrollTo event used to smoothly transition to the top of the page
+  scrollTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+// Scroll to top button function called
+scrollTopBtn();
+// End of scroll to top button
